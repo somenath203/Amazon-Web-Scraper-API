ed087fab04a08dedbb215abdeb92b0037b3e0999
Somenath Choudhury <somenathchoudhury35@gmail.com>
2022-08-22 12:12:19 +0530

    commited

---
+++ public/script.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', () => {
+
+    document.querySelector('.loading_container').classList.add('hide--div')
+
+    document.querySelector('.second_container').classList.remove('hide--div');
+
+});
